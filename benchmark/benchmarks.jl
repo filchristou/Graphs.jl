75d8568f3d4af6d29e5d49f9ea2825fa0dab4f1f
fchrstou <filippos.christou@ikr.uni-stuttgart.de>
2023-11-16 11:19:45 +0100

    All benches

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,23 +1,23 @@
 using BenchmarkTools
 using Graphs
 
-const DIGRAPHS = Dict{String,DiGraph}(
+DIGRAPHS = Dict{String,DiGraph}(
     "complete100" => complete_digraph(100), "path500" => path_digraph(500)
 )
 
-const GRAPHS = Dict{String,Graph}(
+GRAPHS = Dict{String,Graph}(
     "complete100" => complete_graph(100),
     "tutte" => smallgraph(:tutte),
     "path500" => path_graph(500),
 )
 
-const serialbenchmarks = [
+serialbenchmarks = [
     "serial/core.jl",
-    # "serial/connectivity.jl",
-    # "serial/centrality.jl",
-    # "serial/edges.jl",
-    # "serial/insertions.jl",
-    # "serial/traversals.jl",
+    "serial/connectivity.jl",
+    "serial/centrality.jl",
+    "serial/edges.jl",
+    "serial/insertions.jl",
+    "serial/traversals.jl",
 ]
 
 const SUITE = BenchmarkGroup()
@@ -26,8 +26,12 @@ foreach(serialbenchmarks) do bm
     include(bm)
 end
 
-const parallelbenchmarks = [
+parallelbenchmarks = [
     "parallel/egonets.jl",
 ]
+
+foreach(parallelbenchmarks) do bm
+    include(bm)
+end
 
 nothing
